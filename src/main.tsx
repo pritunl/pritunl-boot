19b1c4fdf0a9bdf223bb59a322a23878f86cb8c5
Zachary Huff <zach.huff.386@gmail.com>
2025-09-09 06:59:43 -0400

    Update routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,23 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
+import { Theme } from "@radix-ui/themes"
 import "@radix-ui/themes/styles.css"
 import "./index.css"
-import App from "./App.tsx"
-import { Theme } from "@radix-ui/themes"
+import Register from "./Register.tsx"
+import Manage from "./Manage.tsx"
+import * as Router from "./router"
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<Theme accentColor="yellow" grayColor="sand" appearance="dark">
-			<App/>
+			<Router.RouterProvider>
+				<Router.Route path="/" exact>
+					<Register/>
+				</Router.Route>
+				<Router.Route path="/<bootId>/manage">
+					<Manage/>
+				</Router.Route>
+			</Router.RouterProvider>
 		</Theme>
 	</StrictMode>,
 )
